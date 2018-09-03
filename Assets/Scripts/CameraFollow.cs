232b98541c07652e285f591df2c6081568004c1b
Alberth Salas C <alberthsalascalero@gmail.com>
2018-09-03 10:40:32 -0600

    Movimiento de la camara

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -40,23 +40,27 @@ public class CameraFollow : MonoBehaviour {
 
     IEnumerator TouchScreen()
     {
-        if (touchField.Pressed)
+        if (touchField.TouchDist != Vector2.zero)
         {
-            Debug.Log("Touch dist :" + touchField.TouchDist + ", Camera: " + transform.position);
-
-            if (Mathf.Sign(touchField.TouchDist.x) == -1)
+            //Debug.Log("Touch dist :" + touchField.TouchDist + ", Camera: " + transform.position);
+            if(Mathf.Abs(touchField.TouchDist.x) > Mathf.Abs(touchField.TouchDist.y) &&
+                Mathf.Sqrt(Mathf.Pow(touchField.TouchDist.x, 2) + Mathf.Pow(touchField.TouchDist.y, 2)) > 100)
             {
-                if (displacement.z / Mathf.Abs(displacement.z) != displacement.x / Mathf.Abs(displacement.x))
-                    displacement.z = displacement.z * -1;
+                if (Mathf.Sign(touchField.TouchDist.x) == -1)
+                {
+                    if (displacement.z / Mathf.Abs(displacement.z) != displacement.x / Mathf.Abs(displacement.x))
+                        displacement.z = displacement.z * -1;
+                    else
+                        displacement.x = displacement.x * -1;
+                }
                 else
-                    displacement.x = displacement.x * -1;
-            }
-            else
-            {
-                if (displacement.z / Mathf.Abs(displacement.z) == displacement.x / Mathf.Abs(displacement.x))
-                    displacement.z = displacement.z * -1;
-                else
-                    displacement.x = displacement.x * -1;
+                {
+                    if (displacement.z / Mathf.Abs(displacement.z) == displacement.x / Mathf.Abs(displacement.x))
+                        displacement.z = displacement.z * -1;
+                    else
+                        displacement.x = displacement.x * -1;
+                }
+                touchField.TouchDist = Vector2.zero;
             }
         }
         yield return new WaitForSeconds(delay);
--- Assets/Scripts/FixedTouchField.cs
+++ Assets/Scripts/FixedTouchField.cs
@@ -5,12 +5,7 @@ public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHan
 {
     [HideInInspector]
     public Vector2 TouchDist;
-    [HideInInspector]
-    public Vector2 PointerOld;
-    [HideInInspector]
-    protected int PointerId;
-    [HideInInspector]
-    public bool Pressed;
+    Vector2 first;
 
     // Use this for initialization
     void Start()
@@ -21,36 +16,18 @@ public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     // Update is called once per frame
     void Update()
     {
-        if (Pressed)
-        {
-            if (PointerId >= 0 && PointerId < Input.touches.Length)
-            {
-                TouchDist = Input.touches[PointerId].position - PointerOld;
-                PointerOld = Input.touches[PointerId].position;
-            }
-            else
-            {
-                TouchDist = new Vector2(Input.mousePosition.x, Input.mousePosition.y) - PointerOld;
-                PointerOld = Input.mousePosition;
-            }
-        }
-        else
-        {
-            TouchDist = new Vector2();
-        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        Pressed = true;
-        PointerId = eventData.pointerId;
-        PointerOld = eventData.position;
+        first = eventData.position;
     }
 
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        Pressed = false;
+        TouchDist = eventData.position - first;
+        Debug.Log(TouchDist);
     }
 
 }
